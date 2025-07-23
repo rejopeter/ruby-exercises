19166e38eef298379a9b4a38e720bc8b6e14cf07
Dr. J. R. Rejo Peter, BAMS <218668545+rejopeter@users.noreply.github.com>
2025-07-24 03:55:05 +0530

    done

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -6,11 +6,11 @@
 # To the top, the first line, where the error occurred, and ONLY THEN fix the error
 
 def decrement_smallest_value(nested_array)
-  smallest_value = nested_array.flatten.max
+  smallest_value = nested_array.flatten.min
   nested_array.each do |array|
     array.each do |current_value|
-      if smallest_value > current_value
-        smallest_value = current_valu
+      if smallest_value < current_value
+        smallest_value = current_value
       end
     end
   end
@@ -22,22 +22,23 @@ end
 # Run rspec, let the test fail, and go through the stack trace again
 def increment_greatest_value(nested_array)
   greatest_value = nested_array.flatten.min
+  p greatest_value
   nested_array.each do |array|
     array.each do |current_value|
-      if greatest_value < nil
+      if greatest_value < current_value
         greatest_value = current_value
       end
     end
   end
   greatest_value += 1
 end
-
+increment_greatest_value(nested)
 # This next exercise might look familiar
 # Use p and puts in order to find what's wrong with our method
 
 def isogram?(string)
   original_length = string.length
-  string_array = string.downcase.split
+  string_array = string.downcase.split("")
   unique_length = string_array.uniq.length
   original_length == unique_length
 end
@@ -50,7 +51,6 @@ end
 # Once you find the error, fix it and get the test to pass
 
 def yell_greeting(string)
-  name = string
-  name = name.downcase
-  greeting = "WASSAP, #{name}!"
+  name = string.downcase
+  puts greeting = "WASSAP, #{name}!"
 end
